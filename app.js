33daa4bc654cac5b323d3a0827af47eae5bb2e9d
kresseric <erickykress1@gmail.com>
2022-06-18 07:34:20 +0200

    still working on it

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Full Documentation - https://docs.turbo360.co
+
 const vertex = require('vertex360')({ site_id: process.env.TURBO_APP_ID })
 const express = require('express')
 const app = express(); //   initialize app
--- routes/main.js
+++ routes/main.js
@@ -1,15 +1,16 @@
 const main = require('express');
 const router = main.Router();
+const global = require('../pages/global');
 
 
+//home page
 router.get('/', (req, res, next)=>{
-    const dataTitle={
-        title: "Coffee House Home",
-        description: "Welcome home again! We want  you to stay  happy and fresh. Hope you enjoyed our premium coffee from all over the world! Thank you for joing the  masters!! You are now among us!",
-        Newsletter: "Subscribe us to get every new style or kind of coffee we got from new countries. We know you will love it!"
-    }
-    res.render('home', dataTitle);
-    // res.send('coffee shop web being done!!');
+    res.render('home', global);
+});
+
+//blog page
+router.get('/blog', (req, res, next)=>{
+    res.render('blog', global);
 });
 
 
